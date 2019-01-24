c3bb45318c67b59a768e8df807bf1ba13d3742d4
Samidh Patel <spatel13456@gmail.com>
2019-01-24 03:47:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-def takeANumber (line, name) 
+def take_a_number (line, name) 
   name << line
   puts "Welcome, " + name + ". You are number " + line.length + " in line. "
 end
